fcea322dd91fc744ecf13565d90cfca0bfb1adcd
Brad Honeycutt <owen.honeycutt@gmail.com>
2020-08-23 09:00:53 -0500

    Added final intro scene

---
+++ Assets/Scripts/Amulet.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Amulet : MonoBehaviour
+{
+    //for game intro make it appear our theif picked up an amulet 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("triggered!");
+        Destroy(collision.gameObject);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("triggered!");
+        Destroy(collision.gameObject);
+    }
+}
