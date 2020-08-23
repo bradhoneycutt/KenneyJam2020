42e76e20bf55de33e37aed936601c06bf05a33ca
QuintiumDK <70047130+QuintiumDK@users.noreply.github.com>
2020-08-22 20:33:45 -0500

    Enemy Prefab and AI Movement

---
+++ Assets/Scripts/MovementDirectionEnum.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum MovementDirectionEnum
+{
+    Idle = -1,
+    East = 0,
+    NorthEast = 1,
+    North = 2,
+    NorthWest = 3,
+    West = 4,
+    SouthWest = 5,
+    South = 6,
+    SouthEast = 7,
+    MAX = 8     // move this as max value if adding more directions 
+}
